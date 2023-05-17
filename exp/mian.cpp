b89b342a2502aa6c796b1ea5ec111dc6c7d59a5a
bellday <92672351+bellday@users.noreply.github.com>
2023-05-17 15:23:07 +0900

    Update mian.cpp

--- exp/mian.cpp
+++ exp/mian.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+#include "Exp.h"
+#include "Exp.cpp"
+int main() {
+	exp a(3, 2); 
+	exp b(9); 
+	exp c; 
+
+	cout << a.getValue() << ' ' << b.getValue() << ' ' << c.getValue() << endl;
+	cout << "a의 베이스 " << a.getBase() << ',' << "지수 " << a.getExp() << endl;
+
+	if(a.equals(b)) 
+		cout << "same" << endl;
+	else
+		cout << "not same" << endl;
+}
